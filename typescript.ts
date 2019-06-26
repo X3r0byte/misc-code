8e8fc3a61a93a6b47e5c057ad285561d17e119c8
X3r0byte <41211085+X3r0byte@users.noreply.github.com>
2019-06-26 14:43:11 -0500

    added misc methods

--- typescript.ts
+++ typescript.ts
@@ -10,4 +10,54 @@
       i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
+
+
+
  
+exports.getFilesByRootFolderName = function (req, res) {
+
+  const filesystem = require('fs');
+  let RootFolderName = req.params.RootFolderName;
+  let files;
+  const projectFilesharePath = fileshareRootPath + RootFolderName;
+  // this walks the file tree by given directory and assign all files
+  // to an array _getAllFilesFromFolder
+    var _getAllFilesFromFolder = function (dir) {
+    let results = [];
+    filesystem.readdirSync(dir).forEach(function (file) {
+
+      let name = file.split('.')[0];
+      let type = file.split('.')[1];
+      let subFolderPath = dir;
+      let folderArray = subFolderPath.split('\\');
+      let path = dir + '\\' + file;
+      file = dir + '\\' + file;
+      let stat = filesystem.statSync(file);
+    // let folder = folderArray[folderArray.length - 1];
+      let folder = subFolderPath;
+
+      if (stat && stat.isDirectory()) {
+        results = results.concat(_getAllFilesFromFolder(file))
+      } else {
+        const f = {path: path, type: type, name: name, stats: stat, folder: folder};
+        results.push(f);
+      }
+    });
+    return results;
+  }
+
+  // create the fileshare folder if it doesn't already exist
+  if (!filesystem.existsSync(projectFilesharePath)) {
+    filesystem.mkdirSync(projectFilesharePath);
+  }
+
+  // call the variable from above to get files
+  files = _getAllFilesFromFolder(projectFilesharePath);
+
+  // send the files!
+  if (files != null) {
+    helpers.handleResults(null, files, res);
+  } else {
+    helpers.handleResults(null, {}, res);
+  }
+};
